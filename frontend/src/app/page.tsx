4b52fc45284e6897cb3dd83ccb1be6a7613c1103
johnson-oragui <johnson.oragui@gmail.com>
2025-12-26 22:04:48 +0100

    feat: Add Security to landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
 import Features from '@/components/landing/Features';
 import Hero from '@/components/landing/Hero';
+import Security from '@/components/landing/Security';
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 			<Header user={undefined} />
 			<Hero />
 			<Features />
+			<Security />
 			<Footer />
 		</main>
 	);
