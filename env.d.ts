25a2ccc8d04ede46423b0f7dc363f071cbed8554
johnson-oragui <johnson.oragui@gmail.com>
2025-12-28 10:35:35 +0100

    feat: Update ci

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'staging' | 'test';
-    PORT: number;
-
-    MONGODB_URI: string;
-    DB_NAME: string;
-  }
-}
