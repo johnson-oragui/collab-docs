a8ef46d3ebc86fbdf46eecf0edb32bae378fb623
johnson-oragui <johnson.oragui@gmail.com>
2025-12-29 12:41:30 +0100

    feat: Applied middlewares

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoDbModule } from './database/database.module';
 import { validateEnvConfig } from './config/env.validation';
+import { LoggerMiddleware } from './commons/middlewares/logging.middleware';
+import { CatchAllMiddleware } from './commons/middlewares/catchall.middleware';
+import { UserAgentGuard } from './commons/guards/userAgent.guards';
 
 @Module({
   imports: [
@@ -11,6 +16,17 @@ import { validateEnvConfig } from './config/env.validation';
     MongoDbModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: UserAgentGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(CatchAllMiddleware).forRoutes('*');
+  }
+}
