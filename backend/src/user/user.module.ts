3dd640015dc83ec634a71903886a7b23e0b78e21
johnson-oragui <johnson.oragui@gmail.com>
2026-01-01 22:33:17 +0100

    feat: imported IsEMailUniqueConstraint, PasswordModule, CustomJwtModule

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import UserRepository from './user.repository';
 import UserService from './user.service';
 import UserController from './user.controller';
+import { PasswordModule } from '../password/password.module';
+import { CustomJwtModule } from '../jwt-service/jwt.module';
+import { IsEMailUniqueConstraint } from './dto/user-custom-validator/uniqueEmail.validator';
 
 @Module({
-  providers: [UserRepository, UserService],
+  providers: [UserRepository, UserService, IsEMailUniqueConstraint],
   controllers: [UserController],
-  exports: [UserService, UserRepository],
+  exports: [UserService, UserRepository, IsEMailUniqueConstraint],
+  imports: [PasswordModule, CustomJwtModule],
 })
 export default class UserModule {}
