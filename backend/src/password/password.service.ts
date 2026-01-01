c0f3dc4bc534b20fb7679842b02efe7e45e212a6
johnson-oragui <johnson.oragui@gmail.com>
2026-01-01 23:28:19 +0100

    feat: Update hashing options depending on env

--- backend/src/password/password.service.ts
+++ backend/src/password/password.service.ts
@@ -4,15 +4,17 @@ import * as argon2 from 'argon2';
 
 @Injectable()
 export class PasswordService {
-  NODE_ENV: string | undefined;
+  NODE_ENV: string;
   constructor(private readonly configService: ConfigService) {
-    this.NODE_ENV = this.configService.get<string>('NODE_ENV');
+    this.NODE_ENV = this.configService.get<string>('NODE_ENV', 'development');
   }
   async hash(plain: string): Promise<string> {
     return argon2.hash(plain, {
       type: argon2.argon2id,
-      memoryCost: this.NODE_ENV === 'development' ? 2 ** 12 : 2 ** 16, //64 mb
-      timeCost: this.NODE_ENV === 'development' ? 1 : 3,
+      memoryCost: ['development', 'test'].includes(this.NODE_ENV)
+        ? 2 ** 12
+        : 2 ** 16, //64 mb
+      timeCost: ['development', 'test'].includes(this.NODE_ENV) ? 1 : 3,
       parallelism: 1,
     });
   }
