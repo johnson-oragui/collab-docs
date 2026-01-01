343d6b1a3a691926a6d88664fb60b6d7571292a4
johnson-oragui <johnson.oragui@gmail.com>
2026-01-01 22:46:27 +0100

    feat: Add getMe view

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
-import UserService from './user.service';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { CurrentUser } from '../jwt-service/current-user-decorator';
+import { CustomJwtAuthGuard } from '../jwt-service/jwt.auth.guard';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import type { UserSchema } from './schema/user.schema';
+import { UserDataResponseDto } from './dto/user.data.dto';
+import { plainToInstance } from 'class-transformer';
+import { UserResponseEntity } from './dto/user.create.dto';
 
+@ApiTags('USERS')
 @Controller('api/v1/users')
 export default class UserController {
-  constructor(userService: UserService) {}
+  constructor() {}
+
+  @ApiBearerAuth('bearer')
+  @ApiOkResponse({ type: UserDataResponseDto })
+  @UseGuards(CustomJwtAuthGuard)
+  @Get('/me')
+  @HttpCode(HttpStatus.OK)
+  getMe(@CurrentUser() user: UserSchema): UserDataResponseDto {
+    return new UserDataResponseDto({
+      data: plainToInstance(UserResponseEntity, user, {
+        excludeExtraneousValues: true,
+      }),
+    });
+  }
 }
