b499bf068cb73e60285ad2723e72f7e8b73099cf
johnson-oragui <johnson.oragui@gmail.com>
2026-01-01 22:34:06 +0100

    feat: Add acceptTerms and role fileds

--- backend/src/database/schemas/user.schema.ts
+++ backend/src/database/schemas/user.schema.ts
@@ -1,10 +1,12 @@
-import { ObjectId } from 'mongodb';
+import { Document, ObjectId } from 'mongodb';
 
-export interface UserDocument {
+export interface UserDocument extends Document {
   _id: ObjectId;
   name?: string | null;
   email: string;
   password?: string;
+  role?: 'user' | 'admin';
+  acceptTerms?: boolean;
   createdAt: Date;
   updatedAt: Date;
 }
