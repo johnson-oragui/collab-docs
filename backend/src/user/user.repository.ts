1083ecb0dd203392968f3937a6eeda027456dbc7
johnson-oragui <johnson.oragui@gmail.com>
2026-01-01 22:30:05 +0100

    feat: Add findByEmail method

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,7 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Db } from 'mongodb';
+import { Db, WithId } from 'mongodb';
+import { BaseRepository } from '../database/collections/base.repository';
+import { UserDocument } from '../database/schemas/user.schema';
 
 @Injectable()
-export default class UserRepository {
-  constructor(@Inject('MONGO_CLIENT') private readonly db: Db) {}
+export default class UserRepository extends BaseRepository<UserDocument> {
+  constructor(@Inject('DB') protected readonly db: Db) {
+    super(db, 'cd_users');
+  }
+
+  async findByEmail(email: string): Promise<WithId<UserDocument> | null> {
+    // console.log('running find by email...');
+    return this.collection.findOne({ email });
+  }
 }
